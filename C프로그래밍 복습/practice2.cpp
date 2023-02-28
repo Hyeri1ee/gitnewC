8f4a9b81349e524bc16b85147f65b69e668f4aa2
Hyeri1ee <haerizian10@gmail.com>
2023-02-28 18:44:28 +0900

    c언어 첫 커밋

---
+++ C프로그래밍 복습/practice2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+
+	printf("%c \n", 'a'); //%c는 문자로 출력 a
+	printf("%d \n", 'a'); //%d는 숫자로 출력 97
+	printf("%d \n", 7); //숫자 7 7
+	printf("%c \n", '7'); //문자 7 7
+	printf("%d \n", '7');  //문자 7을 숫자로 출력 55
+	return 0;
+}
